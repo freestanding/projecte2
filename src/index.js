df1699631007e5f56607dfa403db14984c913a0a
josue <josue@gmail.com>
2021-10-23 21:15:45 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const port = 3000;
 async function main() {
-  await app.listen(port);
+  await app.listen(process.env.PORT || port);
   console.log("Server on port", port);
 }
 
